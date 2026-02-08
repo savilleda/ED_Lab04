208552c3c8d48638abe3b321bed3545d01b78197
savilleda <savilleda@correo.url.edu.gt>
2026-02-08 10:59:25 -0600

    Ejercicio 5

--- src/main/java/ed/lab/Main.java
+++ src/main/java/ed/lab/Main.java
@@ -2,66 +2,69 @@ package ed.lab;
 
 public class Main
 {
-    private static final ArrayGenerator<Integer> sortedArrayGenerator = length ->
+    private static final ArrayGenerator<String> sortedArrayGenerator = length ->
     {
-        Integer[]array = new Integer[length];
+        String[]array = new String[length];
+        int width = String.valueOf(Math.max(0, length - 1)).length();
         for(int i = 0; i<length; i++)
         {
-            array[i] = i;
+            array[i] = formatIndex(i, width);
         }
         return array;
     };
-    private static final ArrayGenerator<Integer> invertedArrayGenerator = length ->
+    private static final ArrayGenerator<String> invertedArrayGenerator = length ->
     {
-        Integer[]array = new Integer[length];
+        String[]array = new String[length];
+        int width = String.valueOf(Math.max(0, length - 1)).length();
         for(int i = 0; i<length; i++)
         {
-            array[i] = length-1-i;
+            array[i] = formatIndex(length - 1 - i, width);
         }
         return array;
     };
-    private static final ArrayGenerator<Integer> randomArrayGenerator = length ->
+    private static final ArrayGenerator<String> randomArrayGenerator = length ->
     {
-        Integer[]array = new Integer[length];
+        String[]array = new String[length];
+        int width = String.valueOf(Math.max(0, length - 1)).length();
         java.util.Random random = new java.util.Random();
         for(int i = 0; i<length; i++)
         {
-            array[i] = random.nextInt();
+            array[i] = formatIndex(random.nextInt(length), width);
         }
         return array;
     };
 
-    private static final QuickSort<Integer> highPivotQuickSort = SortingAlgorithms::highPivotQuickSort;
+    private static final QuickSort<String> highPivotQuickSort = SortingAlgorithms::highPivotQuickSort;
 
-    private static final QuickSort<Integer> lowPivotQuickSort = SortingAlgorithms::lowPivotQuickSort;
+    private static final QuickSort<String> lowPivotQuickSort = SortingAlgorithms::lowPivotQuickSort;
 
-    private static final QuickSort<Integer> randomPivotQuickSort = SortingAlgorithms::randomPivotQuickSort;
-    public static QuickSort<Integer> getHighPivotQuickSort() {
+    private static final QuickSort<String> randomPivotQuickSort = SortingAlgorithms::randomPivotQuickSort;
+    public static QuickSort<String> getHighPivotQuickSort() {
         return highPivotQuickSort;
     }
 
-    public static QuickSort<Integer> getLowPivotQuickSort() {
+    public static QuickSort<String> getLowPivotQuickSort() {
         return lowPivotQuickSort;
     }
 
-    public static QuickSort<Integer> getRandomPivotQuickSort() {
+    public static QuickSort<String> getRandomPivotQuickSort() {
         return randomPivotQuickSort;
     }
 
-    public static ArrayGenerator<Integer> getSortedArrayGenerator() {
+    public static ArrayGenerator<String> getSortedArrayGenerator() {
         return sortedArrayGenerator;
     }
 
-    public static ArrayGenerator<Integer> getInvertedArrayGenerator() {
+    public static ArrayGenerator<String> getInvertedArrayGenerator() {
         return invertedArrayGenerator;
     }
 
-    public static ArrayGenerator<Integer> getRandomArrayGenerator() {
+    public static ArrayGenerator<String> getRandomArrayGenerator() {
         return randomArrayGenerator;
     }
 
     public static void main(String[] args) {
-        final SortingTester<Integer> tester = new SortingTester<>();
+        final SortingTester<String> tester = new SortingTester<>();
 
         System.out.println("Ordenando un arreglo ordenado:");
         System.out.println("\tUtilizando el último elemento como pivote: ");
@@ -90,4 +93,8 @@ public class Main
         tester.testSorting(randomArrayGenerator, randomPivotQuickSort);
         System.out.println("================================");
       }
+
+    private static String formatIndex(int value, int width) {
+        return String.format("%0" + width + "d", value);
+    }
 }
--- src/test/java/ed/lab/MainTest.java
+++ src/test/java/ed/lab/MainTest.java
@@ -11,7 +11,7 @@ class MainTest {
         final var sortedArrayGenerator = Main.getSortedArrayGenerator();
         assertThat(sortedArrayGenerator).isNotNull();
 
-        final Integer[] array = sortedArrayGenerator.generate(10);
+        final String[] array = sortedArrayGenerator.generate(10);
 
         assertThat(array)
                 .isNotNull()
@@ -31,7 +31,7 @@ class MainTest {
         final var invertedArrayGenerator = Main.getInvertedArrayGenerator();
         assertThat(invertedArrayGenerator).isNotNull();
 
-        final Integer[] array = invertedArrayGenerator.generate(10);
+        final String[] array = invertedArrayGenerator.generate(10);
 
         assertThat(array)
                 .isNotNull()
@@ -51,14 +51,14 @@ class MainTest {
         final var randomArrayGenerator = Main.getRandomArrayGenerator();
         assertThat(randomArrayGenerator).isNotNull();
 
-        final Integer[] array = randomArrayGenerator.generate(10);
+        final String[] array = randomArrayGenerator.generate(10);
 
         assertThat(array)
                 .isNotNull()
                 .hasSize(10);
 
-        for (Integer integer : array) {
-            assertThat(integer).isNotNull();
+        for (String value : array) {
+            assertThat(value).isNotNull();
         }
     }
 
@@ -70,7 +70,7 @@ class MainTest {
         final var randomArrayGenerator = Main.getRandomArrayGenerator();
         assertThat(randomArrayGenerator).isNotNull();
 
-        final Integer[] array = randomArrayGenerator.generate(10);
+        final String[] array = randomArrayGenerator.generate(10);
         assertThat(array)
                 .isNotNull()
                 .hasSize(10);
@@ -92,7 +92,7 @@ class MainTest {
         final var randomArrayGenerator = Main.getRandomArrayGenerator();
         assertThat(randomArrayGenerator).isNotNull();
 
-        final Integer[] array = randomArrayGenerator.generate(10);
+        final String[] array = randomArrayGenerator.generate(10);
         assertThat(array)
                 .isNotNull()
                 .hasSize(10);
@@ -114,7 +114,7 @@ class MainTest {
         final var randomArrayGenerator = Main.getRandomArrayGenerator();
         assertThat(randomArrayGenerator).isNotNull();
 
-        final Integer[] array = randomArrayGenerator.generate(10);
+        final String[] array = randomArrayGenerator.generate(10);
         assertThat(array)
                 .isNotNull()
                 .hasSize(10);
