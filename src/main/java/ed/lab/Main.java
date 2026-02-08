377386a8ef484e279fa7201f4c77f3f325e84c5c
savilleda <savilleda@correo.url.edu.gt>
2026-02-08 10:52:45 -0600

    Ejercicio 3 y 4 implementando el quicksort

--- src/main/java/ed/lab/Main.java
+++ src/main/java/ed/lab/Main.java
@@ -31,12 +31,11 @@ public class Main
         return array;
     };
 
-    private static final QuickSort<Integer> highPivotQuickSort = null; // Reemplácelo por una referencia a un método
+    private static final QuickSort<Integer> highPivotQuickSort = SortingAlgorithms::highPivotQuickSort;
 
-    private static final QuickSort<Integer> lowPivotQuickSort = null; // Reemplácelo por una referencia a un método
+    private static final QuickSort<Integer> lowPivotQuickSort = SortingAlgorithms::lowPivotQuickSort;
 
-    private static final QuickSort<Integer> randomPivotQuickSort = null; // Reemplácelo por una referencia a un método
-
+    private static final QuickSort<Integer> randomPivotQuickSort = SortingAlgorithms::randomPivotQuickSort;
     public static QuickSort<Integer> getHighPivotQuickSort() {
         return highPivotQuickSort;
     }
---
+++ src/main/java/ed/lab/SortingAlgorithms.java
@@ -0,0 +1,4 @@
+package ed.lab;
+
+public class SortingAlgorithms {
+}
